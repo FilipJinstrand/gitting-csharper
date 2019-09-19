54397b2628c4d44b100e0b9aaeb5c3343fbc6917
Filip Jinstrand <filip@jinstrand.se>
2019-09-19 10:49:03 +0200

    added readme

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace gitting_csharper
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Test");
         }
     }
 }
