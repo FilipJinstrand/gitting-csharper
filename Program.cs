e3df6776b5790e392243eccf8a622aba298d2e1e
Filip Jinstrand <filip@jinstrand.se>
2019-09-19 10:43:49 +0200

    added files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gitting_csharper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
